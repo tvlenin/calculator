85ad97ca4b3dae479dad05eb988a7dd2ad933866
Abraham <abrahamon>
2016-06-18 11:48:39 -0600

    botones en la GUI

--- src/com/GUI/GUI.java
+++ src/com/GUI/GUI.java
@@ -1,9 +1,54 @@
 package com.GUI;
 
 import javax.swing.*;
+import java.awt.event.ComponentAdapter;
 
 /**
- * Created by abrahamon on 17/06/16.
+ * Created by abrahamon on 18/06/16.
  */
-public class GUI {
+public class GUI extends  JFrame{
+
+    public JPanel panelMain;
+    private JPanel panelInput;
+    private JPanel panelButtons;
+    private JButton a7Button;
+    private JButton delButton;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton eButton;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton mulButton;
+    private JButton clearButton;
+    private JButton a1Button;
+    private JButton a0Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton susButton;
+    private JButton equalButton;
+    private JButton espaceButton;
+    private JButton barButton;
+    private JButton addButton;
+
+    private String inputData;
+
+    public GUI(){
+        super("Polynomial Calculator");
+        super.setContentPane(panelMain);
+        super.setBounds(200,100,500,350);                         //The bounds start(X,Y) bounds(Y,X)
+        super.setResizable(false);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super.setVisible(true);
+
+        inputData="";
+
+
+
+    }
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
--- src/com/Main.java
+++ src/com/Main.java
@@ -9,22 +9,26 @@ public class Main {
     public static void main(String[] args) {
 
         //This code open the main window
-        JFrame f = new JFrame("Diabolic Magic Square");
+        //JFrame f = new JFrame("Polynomial Calculator");     //This is the main Frame
+        //GUI userInterface = new GUI();                      //
+        //f.setContentPane(userInterface.panelMain);          //
+        //f.setBounds(200,100,500,350);                         //The bounds start(X,Y) bounds(Y,X)
+        //f.setResizable(false);
+        //f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //Close default method
+        //f.pack();                                           //sizes the frame so that all its contents are at or above their preferred sizes
+        //f.setVisible(true);                                 //
+
         GUI userInterface = new GUI();
-//        f.setContentPane(userInterface.panel1);
-        f.setBounds(10,20,320,485);
-        f.setResizable(false);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
-        System.out.println("Inicio");
+
 
         //While to capture the instructions of the user
-        while (true){
-            System.out.println("--");
+        boolean flag = true;
+        while (flag){
+            System.out.println("Ingres the operation");
             Scanner scan = new Scanner(System.in);
             String s = scan.next();
             System.out.println(s);
-
+            if(s.contains("exit")){flag=false;System.out.println("Command promt finished");}
         }
 
     }
