db6b55901486d04e3f41a6e1e805ca5a20cef1ec
Abraham <abrahamon>
2016-06-18 09:54:28 -0600

    pre pull

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,9 +1,31 @@
 package com;
 
+import javax.swing.*;
+import java.util.*;
+import com.GUI.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.print("ok");
+
+        //This code open the main window
+        JFrame f = new JFrame("Diabolic Magic Square");
+        GUI userInterface = new GUI();
+//        f.setContentPane(userInterface.panel1);
+        f.setBounds(10,20,320,485);
+        f.setResizable(false);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        System.out.println("Inicio");
+
+        //While to capture the instructions of the user
+        while (true){
+            System.out.println("--");
+            Scanner scan = new Scanner(System.in);
+            String s = scan.next();
+            System.out.println(s);
+
+        }
+
     }
 }
